d51d85997170455bbc70796cd65fe451ea6397ce
Nam4802 <58206992+Nam4802@users.noreply.github.com>
2024-04-11 05:42:28 -0400

    added market make and market take instead of just trading with the best orders

--- IMCLobster/IMCLobster.py
+++ IMCLobster/IMCLobster.py
@@ -4,6 +4,8 @@ import string
 
 POSITION_LIMIT = {"STARFRUIT" : 20, "AMETHYSTS" : 20}
 START_POSITION = {"STARFRUIT" : 0, "AMETHYSTS" : 0}
+MAKE_MARGIN = {"STARFRUIT" : 1, "AMETHYSTS" : 1}
+MAKE_VOL = {"STARFRUIT": 5, "AMETHYSTS": 5}
 
 class Trader:
 
@@ -39,6 +41,68 @@ class Trader:
 
         return mavg
 
+    def market_take(self, order_depth, product, position, take_price):
+        take_orders: list[Order] = []
+        new_pos = position
+
+        for ask, vol in order_depth.sell_orders.items():
+            if ask < take_price and (new_pos - vol) < POSITION_LIMIT[product]:
+                #new_pos += min(POSITION_LIMIT[product] - new_pos, - vol)
+                new_pos += - vol
+                take_orders.append(Order(product, ask, - vol))
+
+        for bid, vol in order_depth.buy_orders.items():
+            if bid > take_price and (new_pos - vol) > - POSITION_LIMIT[product]:
+                #new_pos -= min(new_pos + POSITION_LIMIT[product], vol)
+                new_pos -= vol
+                take_orders.append(Order(product, bid, - vol))
+
+        return take_orders, new_pos
+
+
+    def market_make(self, order_depth, product, position, take_price, make_margin, make_vol):
+        make_orders: list[Order] = []
+        best_ask = list(order_depth.sell_orders.keys())[0]
+        best_bid = list(order_depth.buy_orders.keys())[0]
+
+        new_pos = position
+
+        if best_ask > (take_price + make_margin) and new_pos < POSITION_LIMIT[product]:
+            new_pos += min(make_vol, POSITION_LIMIT[product] - new_pos)
+            make_orders.append(Order(product, int(take_price + make_margin), min(make_vol, POSITION_LIMIT[product] - new_pos)))
+
+        if best_bid < (take_price - make_margin) and new_pos > -POSITION_LIMIT[product]:
+            new_pos -= min(make_vol, new_pos + POSITION_LIMIT[product])
+            make_orders.append(Order(product, int(take_price + make_margin), -min(make_vol, new_pos + POSITION_LIMIT[product])))
+
+        return make_orders
+
+    #def market_sell(self, order_depth, product, position, take_price, make_price, makevl_price):
+    #    sell_orders: List[Order] = []
+    #    new_pos = position
+
+    #    for bid, vol in order_depth.buy_orders.items():
+    #        if bid >= take_price and (new_pos - vol) > -POSITION_LIMIT[product]:
+    #            new_pos -= min(new_pos + POSITION_LIMIT[product], vol)
+    #            sell_orders.append(Order(product, bid, - vol))
+
+    #    if new_pos < position_limit[product] / 2:
+    #        buy_orders.append(order(product, make_price, position_limit[product] / 2 - new_pos))
+    #        buy_orders.append(order(product, makevl_price, position_limit[product] / 2))
+
+    #    if new_pos < position_limit[product]:
+    #        buy_orders.append(order(product, make_price, position_limit[product] - new_pos))
+
+        #if new_pos > - (POSITION_LIMIT[product] / 2):
+        #    sell_orders.append(Order(product, make_price, - (POSITION_LIMIT[product] / 2) - new_pos))
+        #    sell_orders.append(Order(product, makevl_price, - (POSITION_LIMIT[product] / 2)))
+
+        #if new_pos > - POSITION_LIMIT[product]:
+        #    sell_orders.append(Order(product, make_price, - POSITION_LIMIT[product] - new_pos))
+
+        #return sell_orders
+
+
     def run(self, state: TradingState):
 
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
@@ -58,26 +122,25 @@ class Trader:
             orders: List[Order] = []
 
             if product == "STARFRUIT":
-                buy_acceptable_price = self.calc_price_ma(data_s)
-                sell_acceptable_price = self.calc_price_ma(data_s)
+                take_price = self.calc_price_ma(data_s)
             elif product == "AMETHYSTS":
-                buy_acceptable_price = 10000
-                sell_acceptable_price = 10000
+                take_price = 10000
+
+            if len(state.position) == 0:
+                prod_position = START_POSITION[product]
+            else:
+                prod_position = state.position[product]
+        
+            [take_orders, make_position] = self.market_take(order_depth, product, prod_position, take_price)
+
+            orders += take_orders
+
+            #make_orders = self.market_make(order_depth, product, make_position, take_price, MAKE_MARGIN[product], MAKE_VOL[product])
+
+            #orders += make_orders
 
             #print("Acceptable price : " + str(acceptable_price))
             #print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-    
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                if int(best_ask) < (buy_acceptable_price):
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                if int(best_bid) > (sell_acceptable_price):
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
             
             result[product] = orders
     
--- IMCLobster/test.py
+++ IMCLobster/test.py
@@ -9,4 +9,5 @@ current_price = list(order_depth.sell_orders.keys())
 
 print("abc" + str(current_price))
 
-print(order_depth.buy_orders[10][-1])
+for ask, vol in order_depth.buy_orders.items():
+    print(str(ask) + ", " + str(vol))
