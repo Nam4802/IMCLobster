d740cd31203a71843b0a2633010bc3666cde3e3d
Nam4802 <58206992+Nam4802@users.noreply.github.com>
2024-04-10 20:39:50 -0400

    Changed current price to mid price Added moving average calculator

--- IMCLobster/test.py
+++ IMCLobster/test.py
@@ -2,8 +2,8 @@ from datamodel import OrderDepth
 from typing import List, Dict
 
 order_depth = OrderDepth()
-order_depth.buy_orders = {10: [7, 6], 9: 5}
-order_depth.sell_orders = {12: -5, 15: -7, 13: -3}
+order_depth.buy_orders = {10: 7, 9: 5}
+order_depth.sell_orders = {12: -5, 13: -3}
 
 current_price = list(order_depth.sell_orders.keys())
 
