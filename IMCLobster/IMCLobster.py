e78b19d97b3aebbcb233aaec448db56cf16afbdf
Nam4802 <58206992+Nam4802@users.noreply.github.com>
2024-04-18 17:18:05 -0400

    current best version

--- IMCLobster/IMCLobster.py
+++ IMCLobster/IMCLobster.py
@@ -1,15 +1,16 @@
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List, Dict
 import string
+import numpy as np
 
-POSITION_LIMIT = {"STARFRUIT" : 20, "AMETHYSTS" : 20, "ORCHIDS": 100}
-START_POSITION = {"STARFRUIT" : 0, "AMETHYSTS" : 0, "ORCHIDS": 0}
-MAKE_MARGIN = {"STARFRUIT" : 2, "AMETHYSTS" : 4, "ORCHIDS": 2}
-MAKE_VOL = {"STARFRUIT": 6, "AMETHYSTS": 6, "ORCHIDS": 6}
+POSITION_LIMIT = {"STARFRUIT" : 20, "AMETHYSTS" : 20, "ORCHIDS": 100, "CHOCOLATE": 250, "STRAWBERRIES": 350, "ROSES": 60, "GIFT_BASKET": 60}
+START_POSITION = {"STARFRUIT" : 0, "AMETHYSTS" : 0, "ORCHIDS": 0, "CHOCOLATE": 0, "STRAWBERRIES": 0, "ROSES": 0, "GIFT_BASKET": 0}
+MAKE_MARGIN = {"STARFRUIT" : 2, "AMETHYSTS" : 4}
+MAKE_VOL = {"STARFRUIT": 6, "AMETHYSTS": 6}
 
 class Trader:
 
-    data = {'STARFRUIT':[5040], 'AMETHYSTS':[10002], 'ORCHIDS': [1200]}
+    data = {"STARFRUIT":[], "AMETHYSTS":[], "ORCHIDS": [], "CHOCOLATE": [], "STRAWBERRIES": [], "ROSES": [], "GIFT_BASKET": []}
 
     # Function to just print a dict containing current mid price of all products
     def mid_price(self, order_depth):
@@ -28,7 +29,6 @@ class Trader:
     def update_data(self, order_depth):
         for product in order_depth:
             mid_price = (list(order_depth[product].sell_orders.keys())[0] + list(order_depth[product].buy_orders.keys())[0]) / 2
-            print('Mid-price ' + product + ': ' + str(mid_price))
             Trader.data[product].append(mid_price)
 
     # Calculate moving average
@@ -41,45 +41,27 @@ class Trader:
 
         return mavg
 
-    def market_take(self, order_depth, product, position, take_price):
-        take_orders: list[Order] = []
-        new_pos = position
+    # Calculate standard deviation
+    def calc_price_std(self, data, std_dur):
 
-        for ask, vol in order_depth.sell_orders.items():
-            if ask < take_price:
-                new_pos += min(POSITION_LIMIT[product] - new_pos, - vol)
-                take_orders.append(Order(product, ask, min(POSITION_LIMIT[product] - new_pos, - vol)))
+        std = np.std([data[i] for i in range(- min(len(data), std_dur), 0)])
 
-        for bid, vol in order_depth.buy_orders.items():
-            if bid > take_price:
-                new_pos -= min(new_pos + POSITION_LIMIT[product], vol)
-                take_orders.append(Order(product, bid, - min(new_pos + POSITION_LIMIT[product], vol)))
-
-        return take_orders, new_pos
-
+        return std
 
     def market_make(self, order_depth, product, position, take_price, make_margin, make_vol):
         make_orders: list[Order] = []
-        #best_ask = list(order_depth.sell_orders.keys())[0]
-        #best_bid = list(order_depth.buy_orders.keys())[0]
 
         new_pos = position
-
-        #if best_ask > (take_price + make_margin) and new_pos < POSITION_LIMIT[product]:
-        #    new_pos += min(make_vol, POSITION_LIMIT[product] - new_pos)
-        #    make_orders.append(Order(product, take_price - make_margin, min(make_vol, POSITION_LIMIT[product] - new_pos)))
-
-        #if best_bid < (take_price - make_margin) and new_pos > -POSITION_LIMIT[product]:
-        #    new_pos -= min(make_vol, new_pos + POSITION_LIMIT[product])
-        #    make_orders.append(Order(product, take_price + make_margin, -min(make_vol, new_pos + POSITION_LIMIT[product])))
 
         if new_pos < POSITION_LIMIT[product]:
             new_pos += min(make_vol, POSITION_LIMIT[product] - new_pos)
             make_orders.append(Order(product, int(take_price - make_margin), min(make_vol, POSITION_LIMIT[product] - new_pos)))
+            
 
         if new_pos > -POSITION_LIMIT[product]:
             new_pos -= min(make_vol, new_pos + POSITION_LIMIT[product])
             make_orders.append(Order(product, int(take_price + make_margin), -min(make_vol, new_pos + POSITION_LIMIT[product])))
+            
 
         return make_orders
 
@@ -91,14 +73,20 @@ class Trader:
         if len(order_depth.buy_orders) != 0:
             best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
             if int(best_bid) > take_price:
-                new_pos -= best_bid_amount
-                basic_orders.append(Order(product, best_bid, -best_bid_amount))
+                
+                basic_orders.append(Order(product, best_bid, - min(new_pos + POSITION_LIMIT[product], best_bid_amount)))
+                #new_pos -= best_bid_amount
+                new_pos -= min(new_pos + POSITION_LIMIT[product], best_bid_amount)
+                
 
         if len(order_depth.sell_orders) != 0:
             best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
             if int(best_ask) < take_price:
-                new_pos += best_ask_amount
-                basic_orders.append(Order(product, best_ask, -best_ask_amount))
+                
+                basic_orders.append(Order(product, best_ask, min(POSITION_LIMIT[product] - new_pos, - best_ask_amount)))
+                #new_pos += best_ask_amount
+                new_pos += min(POSITION_LIMIT[product] - new_pos, - best_ask_amount)
+                
 
         return basic_orders, new_pos
 
@@ -121,10 +109,6 @@ class Trader:
             if best_ask < (best_bid_duck - export_tar - tp_fee / ( - best_ask_amount)) and (new_pos + best_ask_amount) >= - POSITION_LIMIT[product]:
                 conv += best_ask_amount
                 basic_orders.append(Order(product, best_ask, - best_ask_amount))
-
-            #elif best_ask < take_price:
-            #    new_pos -= best_ask_amount
-            #    basic_orders.append(Order(product, best_ask, -best_ask_amount))
     
         if len(order_depth.buy_orders) != 0:
             best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
@@ -133,49 +117,55 @@ class Trader:
                 basic_orders.append(Order(product, best_bid, - best_bid_amount))
 
         return basic_orders, new_pos, conv
+
+    def calc_gift(self, all_order_depths, position):
+        gift_orders: list[Order] = []
+        choco_ord = all_order_depths["CHOCOLATE"]
+        strb_ord = all_order_depths["STRAWBERRIES"]
+        rose_ord = all_order_depths["ROSES"]
+        gift_ord = all_order_depths["GIFT_BASKET"]
+
+        new_pos = position
+
+        sell_gift_check = list(gift_ord.buy_orders.keys())[0] - 4 * list(choco_ord.sell_orders.keys())[0] - 6 * list(strb_ord.sell_orders.keys())[0] - list(rose_ord.sell_orders.keys())[0]
+
+        buy_gift_check = - (list(gift_ord.sell_orders.keys())[0] - 4 * list(choco_ord.buy_orders.keys())[0] - 6 * list(strb_ord.buy_orders.keys())[0] - list(rose_ord.buy_orders.keys())[0])
+
+        if sell_gift_check > 0 and len(gift_ord.buy_orders) != 0:
+            best_bid, best_bid_amount = list(gift_ord.buy_orders.items())[0]
+
+            sell_vol = min(new_pos + POSITION_LIMIT["GIFT_BASKET"], best_bid_amount)
+
+            gift_orders.append(Order("GIFT_BASKET", best_bid, - sell_vol))
+            gift_orders.append(Order("CHOCOLATE", list(choco_ord.sell_orders.keys())[0], 4 * sell_vol))
+            gift_orders.append(Order("STRAWBERRIES", list(strb_ord.sell_orders.keys())[0], 6 * sell_vol))
+            gift_orders.append(Order("ROSES", list(rose_ord.sell_orders.keys())[0], sell_vol))
             
+            new_pos -= sell_vol
 
+        if buy_gift_check > 0 and len(gift_ord.sell_orders) != 0:
+            best_ask, best_ask_amount = list(gift_ord.sell_orders.items())[0]
 
-    #def market_sell(self, order_depth, product, position, take_price, make_price, makevl_price):
-    #    sell_orders: List[Order] = []
-    #    new_pos = position
+            buy_vol = min(POSITION_LIMIT["GIFT_BASKET"] - new_pos, - best_ask_amount)
 
-    #    for bid, vol in order_depth.buy_orders.items():
-    #        if bid >= take_price and (new_pos - vol) > -POSITION_LIMIT[product]:
-    #            new_pos -= min(new_pos + POSITION_LIMIT[product], vol)
-    #            sell_orders.append(Order(product, bid, - vol))
+            gift_orders.append(Order("GIFT_BASKET", best_ask, buy_vol))
+            gift_orders.append(Order("CHOCOLATE", list(choco_ord.buy_orders.keys())[0], - 4 * buy_vol))
+            gift_orders.append(Order("STRAWBERRIES", list(strb_ord.buy_orders.keys())[0], - 6 * buy_vol))
+            gift_orders.append(Order("ROSES", list(rose_ord.buy_orders.keys())[0], - buy_vol))
 
-    #    if new_pos < position_limit[product] / 2:
-    #        buy_orders.append(order(product, make_price, position_limit[product] / 2 - new_pos))
-    #        buy_orders.append(order(product, makevl_price, position_limit[product] / 2))
+            new_pos += buy_vol
 
-    #    if new_pos < position_limit[product]:
-    #        buy_orders.append(order(product, make_price, position_limit[product] - new_pos))
-
-        #if new_pos > - (POSITION_LIMIT[product] / 2):
-        #    sell_orders.append(Order(product, make_price, - (POSITION_LIMIT[product] / 2) - new_pos))
-        #    sell_orders.append(Order(product, makevl_price, - (POSITION_LIMIT[product] / 2)))
-
-        #if new_pos > - POSITION_LIMIT[product]:
-        #    sell_orders.append(Order(product, make_price, - POSITION_LIMIT[product] - new_pos))
-
-        #return sell_orders
-
+        return gift_orders, new_pos
 
     def run(self, state: TradingState):
 
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
         print("Position: " + str(state.position))
         print("Observations: " + str(state.observations))
-        data_s = Trader.data['STARFRUIT']
-        data_a = Trader.data['AMETHYSTS']
-        data_o = Trader.data['ORCHIDS']
-        #print("Last prices: STARFRUIT: " + str(data_s[-1]) + ", AMETHYSTS: " + str(data_a[-1]))
+
+        self.update_data(state.order_depths)
 
         result = {}
-
-        self.mid_price(state.order_depths)
-        self.update_data(state.order_depths)
 
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
@@ -186,33 +176,41 @@ class Trader:
             else:
                 prod_position = state.position[product]
 
-            if product == "STARFRUIT":
-                take_price = self.calc_price_ma(data_s, 5)
-            elif product == "AMETHYSTS":
+            if product == "AMETHYSTS":
                 take_price = 10000
-            elif product == "ORCHIDS":
-                take_price == self.calc_price_ma(data_o, 200)
+            elif product == "STARFRUIT":
+                take_price = self.calc_price_ma(Trader.data[product], 5)
+            elif product == "STRAWBERRIES":
+                take_price = self.calc_price_ma(Trader.data[product], 100)
+            elif product == "ORCHIDS": 
+                take_price = self.calc_price_ma(Trader.data[product], 200)
+            else:
+                take_price = self.calc_price_ma(Trader.data[product], 250)
+
+                conv = 0
         
             if product == "ORCHIDS" and len(state.observations.conversionObservations) != 0:
                 [take_orders, make_position, conv] = self.orchid_conversion(order_depth, product, prod_position, take_price, state.observations)
+                orders += take_orders
+            #elif product == "GIFT_BASKET":
+            #    [gift_orders, take_position] = self.calc_gift(state.order_depths, prod_position)
+            #    [take_orders, make_position] = self.basic_bns(order_depth, product, take_position, take_price)
+            #    orders += gift_orders
+            #    orders += take_orders
+                
+            elif product in ["STARFRUIT", "AMETHYSTS"]:
+                [take_orders, make_position] = self.basic_bns(order_depth, product, prod_position, take_price)
+                make_orders = self.market_make(order_depth, product, prod_position, take_price, MAKE_MARGIN[product], MAKE_VOL[product])
+                orders += take_orders
+                orders += make_orders
             else:
                 [take_orders, make_position] = self.basic_bns(order_depth, product, prod_position, take_price)
-                conv = 0
-            
-            orders += take_orders
-
-            if product != "ORCHIDS":
-                make_orders = self.market_make(order_depth, product, make_position, take_price, MAKE_MARGIN[product], MAKE_VOL[product])
-
-                orders += make_orders
-
-            #print("Acceptable price : " + str(acceptable_price))
-            #print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-            
+                orders += take_orders
+    
             result[product] = orders
     
     
         traderData = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
-        
+
         conversions = conv
         return result, conversions, traderData
--- IMCLobster/test.py
+++ IMCLobster/test.py
@@ -1,37 +1,118 @@
-shell = {'pizza': 1.41, 'wasabi': 0.61, 'snow': 2.08}
-pizza = {'shell': 0.71, 'wasabi': 0.48, 'snow': 1.52}
-wasabi = {'shell': 1.56, 'pizza': 2.05, 'snow': 3.26}
-snow = {'shell': 0.46, 'pizza': 0.64, 'wasabi': 0.3}
 
-allprod = {'shell':shell, 'pizza':pizza, 'wasabi':wasabi, 'snow':snow}
-i = 0
 
-for prod1, rate1 in allprod['shell'].items():
+    def calc_slopeLR(self, data, LR_dur):
+        time = np.arange(min(len(data), LR_dur)) # take time from 0 to N - 1 with N price in data[product]
+        new_data = [data[i] for i in range(- min(len(data), LR_dur), 0)]
+        slope, intercept = np.polyfit(time, new_data, 1) # take slope using statistic lib (allowed in IMC)
+        return slope
 
-    for prod2, rate2 in allprod[prod1].items():
+    def calc_polyfit_extremum(self, data, plf_dur):
+        time = np.arange(min(len(data), plf_dur))
+        new_data = [data[i] for i in range(- min(len(data), plf_dur), 0)]
 
-        #total = rate1 * rate2
-        #if total >= 1 and prod2 == 'shell':
-        #     print(total)
-        #     print('shell ' + prod1 + ' ' + prod2)
+        coeffs = np.polyfit(time, new_data, 5)
 
-        for prod3, rate3 in allprod[prod2].items():
+        x_target = - 1
 
-            total = rate1 * rate2 * rate3
-            if total >= 1 and prod3 == 'shell':
-                 print(total)
-                 print('shell ' + prod1 + ' ' + prod2 + ' ' + prod3)
+        for x in range(min(len(data), plf_dur), 200):
 
-            #for prod4, rate4 in allprod[prod3].items():
+            dy_dx = coeffs[0] * 5 * x**4 + coeffs[1] * 4 * x**3 + coeffs[2] * 3 * x**2 + coeffs[3] * 2 * x + coeffs[4]
 
-            #    total = rate1 * rate2 * rate3 * rate4
-            #    if total >= 1 and prod4 == 'shell':
-            #        print(total)
-            #        print('shell ' + prod1 + ' ' + prod2 + ' ' + prod3 + ' ' + prod4)
+            if dy_dx < 0.1 and dy_dx > -0.1:
+                x_target = x
+                break
 
-                #for prod5, rate5 in allprod[prod4].items():
+        if x_target == - 1 and coeffs[0] > 0:
+            y_target = - 1          # y_target should be higher than current price
+        elif x_target == -1 and coeffs[0] < 0: 
+            y_target = - 2          # y_target should be lower than current price
+        else:
+            y_target = coeffs[0] * x_target**5 + coeffs[1] * x_target**4 + coeffs[2] * x_target**3 + coeffs[3] * x_target**2 + coeffs[4] * x_target + coeffs[5]
+            
+        return y_target
 
-                #    total = rate1 * rate2 * rate3 * rate4 * rate5
-                #    if total >= 1 and total <= 2 and prod5 == 'shell':
-                #        print(total)
-                #        print('shell ' + prod1 + ' ' + prod2 + ' ' + prod3 + ' ' + prod4 + ' ' + prod5)
+    def plf_bns(self, order_depth, product, position, data):
+        plf_orders: list[Order] = []
+
+        new_pos = position
+
+        pred_price = self.calc_polyfit_extremum(data, 100)
+
+        for ask, vol in order_depth.sell_orders.items():
+            if ask > pred_price and pred_price != -1:
+                new_pos += min(POSITION_LIMIT[product] - new_pos, - vol)
+                plf_orders.append(Order(product, ask, min(POSITION_LIMIT[product] - new_pos, - vol)))
+
+        for bid, vol in order_depth.buy_orders.items():
+            if bid < pred_price or pred_price == -1:
+                new_pos -= min(new_pos + POSITION_LIMIT[product], vol)
+                plf_orders.append(Order(product, bid, - min(new_pos + POSITION_LIMIT[product], vol)))      
+                
+        return plf_orders, new_pos
+
+
+    def market_sell(self, order_depth, product, position, take_price, make_price, makevl_price):
+        sell_orders: List[Order] = []
+        new_pos = position
+
+        for bid, vol in order_depth.buy_orders.items():
+            if bid >= take_price and (new_pos - vol) > -POSITION_LIMIT[product]:
+                new_pos -= min(new_pos + POSITION_LIMIT[product], vol)
+                sell_orders.append(Order(product, bid, - vol))
+
+        if new_pos < position_limit[product] / 2:
+            buy_orders.append(order(product, make_price, position_limit[product] / 2 - new_pos))
+            buy_orders.append(order(product, makevl_price, position_limit[product] / 2))
+
+        if new_pos < position_limit[product]:
+            buy_orders.append(order(product, make_price, position_limit[product] - new_pos))
+
+        if new_pos > - (POSITION_LIMIT[product] / 2):
+            sell_orders.append(Order(product, make_price, - (POSITION_LIMIT[product] / 2) - new_pos))
+            sell_orders.append(Order(product, makevl_price, - (POSITION_LIMIT[product] / 2)))
+
+        if new_pos > - POSITION_LIMIT[product]:
+            sell_orders.append(Order(product, make_price, - POSITION_LIMIT[product] - new_pos))
+
+        return sell_orders
+
+    def boll_bns(self, order_depth, product, position, data):
+        boll_orders: list[Order] = []
+
+        ma = self.calc_price_ma(data, 10)
+        std = self.calc_price_std(data, 10)
+
+        boll_upper = ma + 1.5 * std
+        boll_lower = ma - 1.5 * std
+
+        new_pos = position
+
+        if len(order_depth.buy_orders) != 0:
+            best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+            if int(best_bid) > boll_upper and self.calc_slopeLR(data, 10) >= 1:
+                new_pos -= best_bid_amount
+                boll_orders.append(Order(product, best_bid, -best_bid_amount))
+
+        if len(order_depth.sell_orders) != 0:
+            best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+            if int(best_ask) < boll_lower and self.calc_slopeLR(data, 10) <= -1:
+                new_pos += best_ask_amount
+                boll_orders.append(Order(product, best_ask, -best_ask_amount))
+
+        return boll_orders, new_pos
+
+    def market_take(self, order_depth, product, position, take_price):
+        take_orders: list[Order] = []
+        new_pos = position
+
+        for ask, vol in order_depth.sell_orders.items():
+            if ask < take_price:
+                new_pos += min(POSITION_LIMIT[product] - new_pos, - vol)
+                take_orders.append(Order(product, ask, min(POSITION_LIMIT[product] - new_pos, - vol)))
+
+        for bid, vol in order_depth.buy_orders.items():
+            if bid > take_price:
+                new_pos -= min(new_pos + POSITION_LIMIT[product], vol)
+                take_orders.append(Order(product, bid, - min(new_pos + POSITION_LIMIT[product], vol)))
+
+        return take_orders, new_pos
